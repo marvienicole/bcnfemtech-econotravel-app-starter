59a281d0283e3b9297a0cc3987f0a436862fb839
Nicole Uy <marvienicolebtw@hotmail.com>
2021-07-21 15:38:04 +0200

    editing about.jsx file, adding style

--- web-client/src/js/components/about.jsx
+++ web-client/src/js/components/about.jsx
@@ -1,19 +1,21 @@
 import * as React from "react";
 
 export const About = () => <section className="aboutSection">
-    <h2>¿Quiénes somos?</h2>
+        <div className={"aboutContent1"}>
+            <h2 className={"aboutTitle"}>¿Quiénes somos?</h2>
+            <p className={"aboutInfo"}>
+                Nuestra empresa se fundó a raíz de un viaje que nuestra fundadora, Marina Herrán, realizó en bicicleta por
+                Cataluña en el verano de 2001. Durante ese periodo, descubrió que las opciones turísticas que ofrecían ciudades
+                como Barcelona, Girona y Reus dependían de transportes contaminantes y actividades poco respetuosas con la naturaleza
+                y el entorno, algo que causaba un progresivo deterioro de los increíbles parajes naturales de Cataluña.
+                Preocupada por la falta de alternativas eco-friendly, Marina se propuso fundar una agencia de experiencias
+                turísticas donde el respeto por la naturaleza fuera la principal prioridad. Así nació Econotravel Barcelona.
+            </p>
+        </div>
+        <div className={"aboutContent2"}>
+            <p><span className={"contactDetail"}>Teléfono:</span> 93 000 00 00 </p>
+            <p><span className={"contactDetail"}>Correo:</span> info@econotravel.com </p>
+            <p><span className={"contactDetail"}>Dirección:</span> Carrer dels Viatgers, 28, 08000 Barcelona </p>
+        </div>
 
-    <p>
-        Este es un texto de ejemplo que presenta quiénes somos.Este es un texto de ejemplo que presenta quiénes
-        somos.Este es un texto de ejemplo que presenta quiénes somos.Este es un texto de ejemplo que presenta quiénes
-        somos.Este es un texto de ejemplo que presenta quiénes somos.Este es un texto de ejemplo que presenta quiénes
-        somos.Este es un texto de ejemplo que presenta quiénes somos.
-    </p>
-
-    <p>
-        Este es un texto de ejemplo que presenta quiénes somos.Este es un texto de ejemplo que presenta quiénes
-        somos.Este es un texto de ejemplo que presenta quiénes somos.Este es un texto de ejemplo que presenta quiénes
-        somos.Este es un texto de ejemplo que presenta quiénes somos.Este es un texto de ejemplo que presenta quiénes
-        somos.Este es un texto de ejemplo que presenta quiénes somos.
-    </p>
 </section>
